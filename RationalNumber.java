a8b7e9ba92fbce4d31f6d5f4ab481ee02737e85d
Robert Ghazaryan <chessomania@outlook.com>
2026-06-21 14:01:36 +0400

    Implemented normalize private method

---
+++ RationalNumber.java
@@ -0,0 +1,44 @@
+public class RationalNumber {
+
+    private int numerator;
+    private int denominator;
+
+    public RationalNumber (int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero!");
+        }
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+
+        normalize();
+    }
+
+    private void normalize() {
+        if (this.numerator == 0) {
+            this.denominator = 1;
+            return;
+        }
+        if (this.numerator == this.denominator) {
+            this.numerator = 1;
+            this.denominator = 1;
+            return;
+        }
+        if (this.denominator < 0) {
+            this.numerator *= -1;
+            this.denominator *= -1;
+        }
+        int max;
+        if (this.numerator >= this.denominator)
+            max = this.numerator;
+        else
+            max = this.denominator;
+        for (int i = max / 2; i >=2; i--) {
+            if (this.numerator % i == 0 && this.denominator % i == 0) {
+                this.numerator = this.numerator / i;
+                this.denominator = this.denominator / i;
+                return;
+            }
+        }
+    }
+}
